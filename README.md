32285796bff244e3f3ae4463185f2c1bb6c9e6c6
naumshubaev <naum.shubaev@gmail.com>
2022-12-07 13:13:22 +0300

    tests added

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # Дипломный проект
 
 D221204
+
+
+0) Все пароли я положил в репозиторий - мы делали так в дз, будем считать, что и для диплома это допустимо. Но интересно Ваше мнение, как лучше и обязательно ли прятать пароли в рамках дипломного проекта (в конфигурацию тестов например, чтобы они локально хранились?)
+
+1) TODO: Баг-репорты
+
+2) TODO: Тесты БД - отладка (баги у меня в тестах, не в SUT)
+
+## Запуск контейнеров, приложения и тестов
+0) утановить IntellijIdea и Docker 
+1) склонировать данный репозиторий
+2) запуск контейнеров с MySql, PostgreSQL и Node.js использовать команду
+
+``` docker-compose up -d --build ```
+4) запуск приложения:
+    * для запуска под MySQL:
+    ```
+    java -Dspring.datasource.url=jdbc:mysql://localhost:3306/app -jar artifacts/aqa-shop.jar
+    ```
+    * для запуска под PostgreSQL:
+    ```
+    java -Dspring.datasource.url=jdbc:postgresql://localhost:5432/app -jar artifacts/aqa-shop.jar
+    ```
+5) запуск тестов:
+    * для запуска под MySQL:
+   ```
+   gradlew -Ddb.url=jdbc:mysql://localhost:3306/app clean test
+   
+   ```
+    * для запуска под PostgreSQL:
+   ```
+   gradlew -Ddb.url=jdbc:postgresql://localhost:5432/app clean test
---
+++ src/test/java/test/BuyingTripDbTest.java
@@ -0,0 +1,102 @@
+package test;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import data.Card;
+import data.DataGenerator;
+import data.DbUtils;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+import page.CreditPage;
+import page.PaymentPage;
+import page.StartPage;
+
+import java.sql.SQLException;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class BuyingTripDbTest {
+
+    Card validCard = DataGenerator.getValidCard();
+    Card declinedCard = DataGenerator.getDeclinedCard();
+    Card fakeCard = DataGenerator.getFakeCard();
+
+    @BeforeEach
+    public void openPage() throws SQLException {
+        DbUtils.clearTables();
+        String url = System.getProperty("sut.url");
+        open(url);
+    }
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @Test
+    @DisplayName("Should confirm with APPROVED card for Debit")
+    void shouldConfirmPaymentWithValidCard() throws SQLException {
+        StartPage startPage = new StartPage();
+        PaymentPage paymentPage = startPage.goToPaymentPage();
+        paymentPage.fillData(validCard);
+        paymentPage.notificationOkIsVisible();
+        assertEquals("APPROVED", DbUtils.findPaymentStatus());
+    }
+
+    @Test
+    @DisplayName("Should confirm with APPROVED card for credit")
+    void shouldConfirmCreditWithValidCard() throws SQLException {
+        StartPage startPage = new StartPage();
+        CreditPage creditPage = startPage.goToCreditPage();
+        creditPage.fillData(validCard);
+        creditPage.notificationOkIsVisible();
+        assertEquals("APPROVED", DbUtils.findCreditStatus());
+    }
+
+    @Test
+    @DisplayName("Should not confirm with DECLINED card for Debit")
+    void shouldNotConfirmPaymentWithDeclinedCard() throws SQLException {
+        StartPage startPage = new StartPage();
+        PaymentPage paymentPage = startPage.goToPaymentPage();
+        paymentPage.fillData(declinedCard);
+        paymentPage.notificationErrorIsVisible();
+        assertEquals("DECLINED", DbUtils.findPaymentStatus());
+    }
+
+    @Test
+    @DisplayName("Should not confirm with DECLINED card for Credit")
+    void shouldNotConfirmCreditWithDeclinedCard() throws SQLException {
+        StartPage startPage = new StartPage();
+        CreditPage creditPage = startPage.goToCreditPage();
+        creditPage.fillData(declinedCard);
+        creditPage.notificationErrorIsVisible();
+        assertEquals("DECLINED", DbUtils.findCreditStatus());
+    }
+
+    @Test
+    @DisplayName("Should not confirm with FAKE card for Debit")
+    void shouldNotConfirmPaymentWithFakeCard() throws SQLException {
+        DbUtils.clearTables();
+        StartPage startPage = new StartPage();
+        PaymentPage paymentPage = startPage.goToPaymentPage();
+        paymentPage.fillData(fakeCard);
+        paymentPage.notificationErrorIsVisible();
+        assertEquals("0", DbUtils.countRecords());
+    }
+
+    @Test
+    @DisplayName("Should not confirm with FAKE card for Credit")
+    void shouldNotConfirmCreditWithFakeCard() throws SQLException {
+        StartPage startPage = new StartPage();
+        CreditPage creditPage = startPage.goToCreditPage();
+        creditPage.fillData(fakeCard);
+        creditPage.notificationErrorIsVisible();
+        assertEquals("0", DbUtils.countRecords());
+    }
+}
